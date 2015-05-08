0255a1e84979d9e7ce702b294ffaf1a6da677a1c
Timo Betcke <timo.betcke@gmail.com>
2015-05-08 23:56:48 +0100

    Removed Alberta and added foamgrid

--- dune-grid.rb
+++ dune-grid.rb
@@ -4,10 +4,8 @@ class DuneGrid < Formula
   sha256 "f565d3c2562275cba317adef74f75b0a4f6f130abf4e9e1c34712bc9ab63ab03"
 
   depends_on "cmake"
-  depends_on "libtool"
   depends_on "bempp/bempp/dune-common"
   depends_on "bempp/bempp/dune-geometry"
-  depends_on "alberta"
 
   patch :p1 do
     url "http://www.bempp.org/files/dune/dune_grid_cmake.patch"
