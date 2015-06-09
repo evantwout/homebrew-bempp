6d4aef98e96732f818363010277cf9af72b6aee2
Timo Betcke <timo.betcke@gmail.com>
2015-06-09 23:07:12 +0100

    Updated dune-alugrid

--- dune-alugrid.rb
+++ dune-alugrid.rb
@@ -1,17 +1,12 @@
 class DuneAlugrid < Formula
   homepage "http://users.dune-project.org/projects/dune-alugrid"
-  url "http://users.dune-project.org/repositories/projects/dune-alugrid.git", :using => :git, :revision => "95c07ef8c80c2ea6f19c441eedf997507e40cde4"
+  url "https://github.com/bempp/dune-alugrid.git"
 
   version "2.3-dev"
 
   depends_on "cmake"
   depends_on "bempp/bempp/dune-grid"
 
-  patch :p1 do
-    url "https://github.com/bempp/homebrew-patches/raw/master/alugrid_cmake.patch"
-    sha256 "7710e8ab5855b164c1e2b9b650ebdb1ff8d810e2a4dd3e219ee44d7ef97075dd"
-  end
-    
   def install
 
     mkdir "build" do
